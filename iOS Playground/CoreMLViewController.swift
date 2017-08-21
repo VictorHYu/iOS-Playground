f5ba064fe4a31424f90ac41643422e566ccdbd53
VictorHYu <victor.yu.canada@gmail.com>
2017-08-21 00:59:56 -0400

    Add image classification using MobileNet model

--- iOS Playground/CoreMLViewController.swift
+++ iOS Playground/CoreMLViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CoreML
 
 class CoreMLViewController: UIViewController, UINavigationControllerDelegate {
+    
+    var model: MobileNet!
     
     //MARK: Properties
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var predictionLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        model = MobileNet()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +52,48 @@ class CoreMLViewController: UIViewController, UINavigationControllerDelegate {
 extension CoreMLViewController: UIImagePickerControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true)
+        predictionLabel.text = "Analyzing Image..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 224, height: 224), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 224, height: 224))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+
+        guard let prediction = try? model.prediction(image: pixelBuffer!) 
+            else {
+            return
+        }
+        
+        predictionLabel.text = "I think this is a \(prediction.classLabel)."
     }
 }
 
